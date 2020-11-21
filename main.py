23e5fe151cf21a0aa9ae450f3fc525c06da16361
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-11-21 01:56:08 -0500

    successful read of db

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
+
 import sys
 import networkx as nx
+import psycopg2
 import matplotlib.pyplot as plt
+
+try:
+    connection = psycopg2.connect(user = 'root', password = 'root', host = '127.0.0.1', port = '5432', database = 'c19contact')
+except (Exception, psycopg2.Error) as error :
+    print(error)
+    sys.exit()
+
+cursor = connection.cursor()
+
+cursor.execute('select * from People;')
+record = cursor.fetchall()
+print(record)
 
 # construct graph
 # g = nx.DiGraph()
@@ -13,9 +27,9 @@ import matplotlib.pyplot as plt
 # # draw graph
 # nx.draw_networkx(g,font_color='white')
 
-# plt.savefig("graph.png") # save graph image
+# plt.savefig('graph.png') # save graph image
 
-input = ''
-while input != 'quit' :
-    # tokenize input
-    input = sys.stdin.readline()[:-1].split() # chomp and split
+# input = [''] 
+# while input[0] != 'quit' :
+#     # tokenize input
+#     input = sys.stdin.readline()[:-1].split() # chomp and split
