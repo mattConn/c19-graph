7c1a5b9f84640b71f22c54c112b4f9a9d1d1f6a9
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-12-12 22:09:35 -0500

    WIP adding functionality to change infected status

--- static/nodehover.js
+++ static/nodehover.js
@@ -10,21 +10,27 @@ const handleNodeHover = (node,tooltip) => {
             .duration(50)
             .style('opacity', 1);
 
-        tooltip.html(`<p>${d.name}, ${d.age}</p>${d.infected ? '<p>Infected</p>' : ''}`)
+        tooltip.html(`<p>${d.name}, ${d.age}</p>${d.infected ? '<p>Infected</p>' : ''} <button onClick="${changeStatus(d)}"> Change infected status </button>`)
             .style('left', (d3.event.pageX + 10) + 'px')
-            .style('top', (d3.event.pageY - 15) + 'px');
+            .style('top', (d3.event.pageY - 15) + 'px')
+        
 
     })
 
-    node.on('mouseout', function (d, i) {
-        d3.select(this).transition()
-            .duration('50')
-            .attr('opacity', '1');
+    // node.on('mouseout', function (d, i) {
+    //     d3.select(this).transition()
+    //         .duration('50')
+    //         .attr('opacity', '1');
 
-        tooltip.transition()
-            .duration(50)
-            .style('opacity', 0);
-    })
+    //     tooltip.transition()
+    //         .duration(50)
+    //         .style('opacity', 0);
+    // })
+
 };
+
+function changeStatus(d){
+    console.log(d)
+}
 
 export default handleNodeHover;
